765c6d5a269bb06f5d235a113ce57eebf851ea77
Akshaydevsv21 <akshaydevsv21@gmail.com>
2023-03-15 08:16:00 +0530

    Modified MoveZeros program

--- DSA_Training_Project/src/com/devHub/Arrays/MoveZeros.java
+++ DSA_Training_Project/src/com/devHub/Arrays/MoveZeros.java
@@ -1,13 +1,7 @@
 package com.devHub.Arrays;
-
+import com.devHub.Arrays.ReverseArray;
 public class MoveZeros {
 	
-	public static void printArray(int[] arr) {
-		for(int i=0;i<arr.length;i++) {
-			System.out.print(arr[i]+" ");
-		}
-		System.out.println();
-	}
 	
 	public int[] moveZero(int[] arr) {
 		int j=0;
@@ -29,10 +23,10 @@ public class MoveZeros {
 		int[] arr= {4,7,0,5,2,0,9,73,0,86};
 		MoveZeros mz=new MoveZeros();
 		System.out.println("Initial Array State:-");
-		printArray(arr);
+		ReverseArray.printArray(arr);
 		mz.moveZero(arr);
 		System.out.println("After Moving Zeroes to End Array State:-");
-		printArray(arr);
+		ReverseArray.printArray(arr);
 ;
 	}
 
