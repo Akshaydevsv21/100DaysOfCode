b5765169acaf252a45aa68dc5b474891a4f16f8b
Akshaydevsv21 <akshaydevsv21@gmail.com>
2023-03-17 08:17:35 +0530

    Added Find Missing Number in Array program

---
+++ DSA_Training_Project/src/com/devHub/Arrays/MissingNumber.java
@@ -0,0 +1,24 @@
+/* Program to find Missing Number in an Array with range 1 to n
+* Dated - 17 March 2023
+* Author - Akshay Dev S V */ 
+
+package com.devHub.Arrays;
+
+public class MissingNumber {
+	
+	public static int findMissingNumber(int[] arr) {
+		int n=arr.length+1;
+		int sum=n*(n+1)/2;
+		for(int num:arr) {
+			sum=sum-num;
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		int[] arr=new int[] {1,4,6,7,3,2,5,9};
+		System.out.println("Missing Number in Array:- "+findMissingNumber(arr));
+
+	}
+
+}
