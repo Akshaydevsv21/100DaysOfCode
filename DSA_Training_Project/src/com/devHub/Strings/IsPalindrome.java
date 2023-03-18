41aa6b1f39b5044ea3e4e2f13200b4a27a027c99
Akshaydevsv21 <akshaydevsv21@gmail.com>
2023-03-18 14:05:37 +0530

    Added Palindrome Check for a given String

---
+++ DSA_Training_Project/src/com/devHub/Strings/IsPalindrome.java
@@ -0,0 +1,35 @@
+/* Program to check if a given String is a Palindrome
+ *  Dated - 18 March 2023
+* Author - Akshay Dev S V */ 
+
+package com.devHub.Strings;
+
+public class IsPalindrome {
+	
+	public boolean isPalindrome(String str) {
+		char[] chstring=str.toCharArray();
+		int start=0;
+		int end=str.length()-1;
+		
+		while(start<end) {
+			if(chstring[start]!=chstring[end]) {
+				return false;
+			}
+			start++;
+			end--;
+		}
+		return true;
+	}
+	public static void main(String[] args) {
+		String str="Malayalam";
+		IsPalindrome iP=new IsPalindrome();
+		if(iP.isPalindrome(str.toLowerCase())) {
+			System.out.println("String "+str+" is a Palindrome");
+		}
+		else {
+			System.out.println("String "+str+" is Not a Palindrome");
+		}
+
+	}
+
+}
