35354393e4b3be982d5eb770c1b32af66641b392
Akshaydevsv21 <akshaydevsv21@gmail.com>
2023-03-19 19:43:04 +0530

    Added Linkedlist Package

---
+++ DSA_Training_Project/src/com/devHub/LinkedList/SinglyLinkedList.java
@@ -0,0 +1,21 @@
+package com.devHub.LinkedList;
+
+public class SinglyLinkedList {
+	
+	public ListNode head;
+	
+	private static class ListNode {
+		private int data;
+		private ListNode next;
+		
+		public ListNode(int data) {
+			this.data=data;
+			this.next=next;
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
