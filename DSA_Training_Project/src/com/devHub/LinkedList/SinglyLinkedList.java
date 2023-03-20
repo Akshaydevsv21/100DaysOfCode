e02c84cad0af9c7c0ace59ada80e1c8988bf6656
Akshaydevsv21 <akshaydevsv21@gmail.com>
2023-03-20 23:13:34 +0530

    Added SinglyLinkedList Elements creation and Display logic

--- DSA_Training_Project/src/com/devHub/LinkedList/SinglyLinkedList.java
+++ DSA_Training_Project/src/com/devHub/LinkedList/SinglyLinkedList.java
@@ -8,14 +8,47 @@ public class SinglyLinkedList {
 		private int data;
 		private ListNode next;
 		
+		//parameterized constructor
 		public ListNode(int data) {
 			this.data=data;
 			this.next=next;
-		}
+		}	
 	}
+	
+	public void display() {
+		ListNode current=head;
+		while(current!=null) {
+			System.out.print(current.data+"-->");
+			current=current.next;
+		}
+		System.out.print("null");
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		SinglyLinkedList sll=new SinglyLinkedList();
+		sll.head=new ListNode(5);
+		ListNode second=new ListNode(7);
+		ListNode third=new ListNode(4);
+		ListNode fourth=new ListNode(10);
+		ListNode fifth=new ListNode(23);
+		
+		sll.head.next=second;
+		second.next=third;
+		third.next=fourth;
+		fourth.next=fifth;
+		
+		//5-->7-->4-->10-->23-->null
+		sll.display();
+		
+		
 
 	}
 
 }
+
+
+/*  Output
+ *----------------------------
+ * 5-->7-->4-->10-->23-->null
+ */
