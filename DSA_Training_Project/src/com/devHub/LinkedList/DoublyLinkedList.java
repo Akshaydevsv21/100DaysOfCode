3aa8878cbf476bab847460f8a11f34969ee841e6
Akshaydevsv21 <akshaydevsv21@gmail.com>
2023-04-10 08:32:50 +0530

    Added DoublyLinkedList logic

---
+++ DSA_Training_Project/src/com/devHub/LinkedList/DoublyLinkedList.java
@@ -0,0 +1,23 @@
+package com.devHub.LinkedList;
+
+public class DoublyLinkedList {
+	
+	private ListNode head;
+	private ListNode tail;
+	private int length;
+	
+	private class ListNode{
+		private int data;
+		private ListNode previous;
+		private ListNode next;
+		
+		public ListNode(int data) {
+			this.data=data;
+		}
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
