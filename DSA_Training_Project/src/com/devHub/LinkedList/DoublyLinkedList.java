ab003a2f9fd945839cfabda87377dc26c735f390
Akshaydevsv21 <akshaydevsv21@gmail.com>
2023-06-03 16:33:27 +0530

    Added DoublyLinkedlist Print elements logic

--- DSA_Training_Project/src/com/devHub/LinkedList/DoublyLinkedList.java
+++ DSA_Training_Project/src/com/devHub/LinkedList/DoublyLinkedList.java
@@ -1,3 +1,6 @@
+/* Program to Implement DoublyLinkedList Elements creation and Display logic
+ *  Dated - 3 June 2023
+* Author - Akshay Dev S V   */ 
 package com.devHub.LinkedList;
 
 public class DoublyLinkedList {
@@ -30,12 +33,26 @@ public class DoublyLinkedList {
 		return length;
 	}
 	
+	public void insertLast(int value) {
+		ListNode lt=new ListNode(value);
+		if(isEmpty()) {
+			head=lt;
+		}
+		else {
+			tail.next=lt;
+		}
+		lt.previous=tail;
+		tail=lt;
+		length++;
+	}
+	
 	public void displayForward() {
 		if(head==null) {
 			return;
 		}
 		
 		ListNode current=head;
+		System.out.println("Doubly Linkedlist in Forward direction");
 		while(current!=null) {
 			System.out.print(current.data+"-->");
 			current=current.next;
@@ -49,6 +66,7 @@ public class DoublyLinkedList {
 		}
 		
 		ListNode current=tail;
+		System.out.println("Doubly Linkedlist in Backward direction");
 		while(current!=null) {
 			System.out.print(current.data+"-->");
 			current=current.previous;
@@ -57,7 +75,23 @@ public class DoublyLinkedList {
 	}
 
 	public static void main(String[] args) {
-
+		DoublyLinkedList dll=new DoublyLinkedList();
+		dll.insertLast(1);;
+		dll.insertLast(4);
+		dll.insertLast(7);
+		dll.insertLast(34);
+		dll.insertLast(47);
+		dll.displayBackward();
+		dll.displayForward();
+		
 	}
 
 }
+
+/*  Output
+ *----------------------------
+ * Doubly Linkedlist in Backward direction
+ * 47-->34-->7-->4-->1-->null
+ * Doubly Linkedlist in Forward direction
+ * 1-->4-->7-->34-->47-->null
+ **/
