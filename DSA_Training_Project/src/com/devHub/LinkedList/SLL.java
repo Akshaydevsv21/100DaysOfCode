c67619a2cff33872cc9e23f2f34bd38153e3462b
Akshaydevsv21 <akshaydevsv21@gmail.com>
2024-03-16 06:49:08 +0530

    Added code to delete a node from SLL when it equals a key value passed as parameter

--- DSA_Training_Project/src/com/devHub/LinkedList/SLL.java
+++ DSA_Training_Project/src/com/devHub/LinkedList/SLL.java
@@ -185,6 +185,26 @@ public class SLL {
 		return head;
 	}
 	
+	//Method to delete a node which equals to a key value passed as parameter
+	
+	public void deleteNode(int key) {
+		ListNode current=head;
+		ListNode temp=null;
+		
+		if(current!=null && current.data==key) {
+			head=current.next;
+			return;
+		}
+		while(current!=null && current.data!=key) {
+			temp=current;
+			current=current.next;
+		}
+		if(current==null) {
+			return;
+		}
+		temp.next=current.next;
+	}
+	
 	public static void main(String[] args) {
 		
 		SLL sl=new SLL();
@@ -218,6 +238,8 @@ public class SLL {
 		sl.removeDuplicatesfromSLL();
 		sl.display();
 		sl.insertnewNodeinSortedSLL(new ListNode(40));
+		sl.display();
+		sl.deleteNode(60);
 		sl.display();
 	}
 
